b815dd5b3480306c01f7663b64a53b3ab2cfa6af
aurelappius <appiusa@student.ethz.ch>
2021-12-15 16:02:41 +0100

    working fake publisher

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -22,77 +22,25 @@ int main(int argc, char *argv[]) {
   set_parameters(paths::parameters_path);
   const int N = parameters::objects.size();  // Number of Objects
 
-  ////////////////////////////////////////////////////////////
   // FastDDS objects
 
   // Create participant. Arguments-> Domain id, QOS name
   DefaultParticipant dp(0, "mocap_publisher");
 
-  // Create DDS publisher vector
-  std::vector<DDSPublisher> mocap_pub;
+  // Create publisher with msg type
+  DDSPublisher mocap_pub(idl_msg::MocapPubSubType(), "mocap_pose",
+                         dp.participant());
 
-  // Create Mocap Message Vector
-  std::vector<cpp_msg::Mocap> mocap_msg(N);
+  // .idl message
+  cpp_msg::Mocap mocap_msg;
 
-  // Initialize each Element of the vector
-  for (int i = 0; i < N; i++) {
-    std::string topic = parameters::topic_prefix + parameters::objects.at(i);
-    mocap_pub.push_back(
-        DDSPublisher(idl_msg::MocapPubSubType(), topic, dp.participant()));
-    mocap_pub.at(i).init();
+  // Initalize mocap_publisher
+  mocap_pub.init();
+
+  mocap_msg.pose.position.x = 1;
+  mocap_msg.pose.position.y = 1;
+  mocap_msg.pose.position.z = 1;
+  while (true) {
+    mocap_pub.publish(mocap_msg);
   }
-
-  // ////////////////////////////////////////////////////////
-  // for (int i = 0; i < N; i++) {
-  //   mocap_msg.at(i).header.timestamp = _Output_GetFrameNumber.FrameNumber;
-  // }
-  // ////////////////////////////////////////////////////////
-
-  // ///////////////////////////////////////////////////////////////
-  // for (int i = 0; i < N; i++) {
-  //   mocap_msg.at(i).latency = MyClient.GetLatencyTotal().Total;
-  // }
-
-  // ////////////////////////////////////////////
-  // for (int i = 0; i < N; i++) {
-  //   if (SubjectName.compare(parameters::objects.at(i)) == 0) {
-  //     mocap_msg.at(i).header.id = SubjectName;
-  //   }
-  // }
-  // ////////////////////////////////////////////
-
-  // // Capture position in FastDDS message
-  // ////////////////////////////////////////////
-  // for (int i = 0; i < N; i++) {
-  //   if (SubjectName.compare(parameters::objects.at(i)) == 0) {
-  //     mocap_msg.at(i).pose.position.x =
-  //         _Output_GetSegmentGlobalTranslation.Translation[0] / 1000.0;
-  //     mocap_msg.at(i).pose.position.y =
-  //         _Output_GetSegmentGlobalTranslation.Translation[1] / 1000.0;
-  //     mocap_msg.at(i).pose.position.z =
-  //         _Output_GetSegmentGlobalTranslation.Translation[2] / 1000.0;
-  //   }
-  // }
-
-  // ////////////////////////////////////////////
-  // for (int i = 0; i < N; i++) {
-  //   if (SubjectName.compare(parameters::objects.at(i)) == 0) {
-  //     mocap_msg.at(i).pose.orientation_quat.x =
-  //         _Output_GetSegmentGlobalRotationQuaternion.Rotation[0];
-  //     mocap_msg.at(i).pose.orientation_quat.y =
-  //         _Output_GetSegmentGlobalRotationQuaternion.Rotation[1];
-  //     mocap_msg.at(i).pose.orientation_quat.z =
-  //         _Output_GetSegmentGlobalRotationQuaternion.Rotation[2];
-  //     mocap_msg.at(i).pose.orientation_quat.w =
-  //         _Output_GetSegmentGlobalRotationQuaternion.Rotation[3];
-  //   }
-  // }
-
-  // // Publish data
-  // for (int i = 0; i < N; i++) {
-  //   if (SubjectName.compare(parameters::objects.at(i)) == 0) {
-  //     mocap_pub.at(i).publish(mocap_msg.at(i));
-  //   }
-  // }
-  // ////////////////////////////////////
 }
